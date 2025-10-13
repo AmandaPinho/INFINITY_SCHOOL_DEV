f15a63c608356edf6960c10191d00ef3f4118fc4
Amanda Pinho <95372366+AmandaPinho@users.noreply.github.com>
2025-10-13 19:09:19 -0300

    Create prova04python.py

---
+++ LOGICA_PYTHON/prova04python.py
@@ -0,0 +1,9 @@
+# Crie uma função chamada media que receberá três números como argumentos. 
+# Esta função deve calcular a média aritmética desses três números. 
+# Para fazer isso, some os três números e, em seguida, divida o resultado por três. 
+# Por fim, a função deve retornar o valor da média aritmética calculada.
+
+def media(a, b, c):
+    return (a+b+c)/3
+
+print(media(5,7,7))
